2555edeec5a162946a347d6b9d8fce381bcb8ffe
Training <Training@DESKTOP-A16S33J>
2025-02-20 17:32:52 +0530

    API gateway

---
+++ gatewaybatch6/src/main/java/com/gatewaybatch6/Gatewaybatch6Application.java
@@ -0,0 +1,13 @@
+package com.gatewaybatch6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gatewaybatch6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gatewaybatch6Application.class, args);
+	}
+
+}
---
+++ gatewaybatch6/src/test/java/com/gatewaybatch6/Gatewaybatch6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gatewaybatch6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gatewaybatch6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
